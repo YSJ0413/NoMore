332418ff3939fc6c5d4b0dd0fa32475012873836
hasechan <gktpcks401@naver.com>
2017-08-16 15:46:41 +0900

    MainView Patch
    메인뷰 만듬

---
+++ Assets/Scripts/MainView/Main.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    public void Click()
+    {
+        Debug.Log("SS");
+        Application.LoadLevel("Test");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //public GUISkin scene;
+
+    //private void OnGUI()
+    //{
+    //    GUI.skin = scene;
+    //    if(GUI.Button(new Rect(500,500,80,80),"") == true)
+    //    {
+    //        Application.LoadLevel("Test");
+    //    }
+    //}
+
